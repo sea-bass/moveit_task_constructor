045e84dccbfb2d2057590fbdcb3a70dd84c9fcd2
Abishalini <abishalini.sivaraman@picknik.ai>
2023-03-16 17:21:12 -0600

    Handle failed trajectory in connect stage

--- core/src/stages/connect.cpp
+++ core/src/stages/connect.cpp
@@ -161,7 +161,13 @@ void Connect::compute(const InterfaceState& from, const InterfaceState& to) {
 
 		robot_trajectory::RobotTrajectoryPtr trajectory;
 		success = pair.second->plan(start, end, jmg, timeout, trajectory, path_constraints);
-		sub_trajectories.push_back(trajectory);  // include failed trajectory
+
+		if (success) {
+			sub_trajectories.push_back(trajectory);  
+		}
+		else {
+			sub_trajectories.push_back(nullptr); 
+		}
 
 		if (!success)
 			break;
